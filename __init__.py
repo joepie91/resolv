db2a07dfec475dd54da317a8da63ed08c0d5626b
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-06-05 04:37:20 +0200

    Added 'intelligent' resolver

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+import re
 from resolvers import *
 
-
+def resolve(url):
+	if re.match("https?:\/\/(www\.)?putlocker\.com", url) is not None:
+		return putlocker.resolve(url)
+	elif re.match("https?:\/\/(www\.)?sockshare\.com", url) is not None:
+		return sockshare.resolve(url)
+	else:
+		return {}
