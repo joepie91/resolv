ae2b48a36826231310cc205d90b542e4367c9bc9
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-06-08 02:51:42 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='resolv',
+      version='1.0.0',
+      description='Module for resolving URLs from filehosters, video hosters, and other content hosters',
+      author='Sven Slootweg',
+      author_email='resolv@cryto.net',
+      url='http://cryto.net/resolv',
+      packages=['resolv', 'resolv.resolvers'],
+      provides='resolv'
+     )
