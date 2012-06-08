de3b3217699d5c6e136cc70e6a4d31e52a5479dc
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-06-08 06:50:35 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='resolv',
-      version='1.0.1',
+      version='1.0.2',
       description='Module for resolving URLs from filehosters, video hosters, and other content hosters',
       author='Sven Slootweg',
       author_email='resolv@cryto.net',
